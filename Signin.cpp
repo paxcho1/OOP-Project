b400026f64b0886fb750e47fe22df65882b3248c
paxcho1 <paxcho1@naver.com>
2018-11-21 13:32:53 +0900

    Add files via upload

--- Signin.cpp
+++ Signin.cpp
@@ -1,8 +1,7 @@
 #include "Signin.h"
 
 
-
-Signin::Signin(SOCKET client)
+Signin::Signin(SOCKET client):tool(client)
 {
 }
 
@@ -11,7 +10,7 @@ Signin::~Signin()
 {
 }
 
-void Signin::in(SOCKET client)
+int Signin::in(SOCKET client)
 {
 	char id[MAX_BUFFER_SIZE];
 	char password[MAX_BUFFER_SIZE];
@@ -19,24 +18,56 @@ void Signin::in(SOCKET client)
 	int Password_Bytein;
 	//id 중복검사
 	int overlap = 0;
+	vector<string> V_id;
+	set<pair<string, string>> user_Info;
 	do {
-		
-		{
-		//id receive
-		//에러 메세지
-		//id map 검색
-		//if () {//id가 없을때
-			overlap = 1;
-			//id가 없다는 메세지 send
-		//}
-		//else if {//id가 있을때
-
-			//id 중복 메세지 send
-
+		Id_Bytein = tool::Recv(client, id);
+		if (Id_Bytein <= 0) {
+			return -1;
 		}
+		ifstream rFile("Id_index.bin", ios_base::binary);
+		unsigned vsize;
+
+		rFile.read(reinterpret_cast<char*>(&vsize), sizeof(unsigned));
+		rFile.read(reinterpret_cast<char*>(&V_id[0]), vsize * sizeof(string));
+		rFile.close();
+		//in_index 파일 불러오기(binary)
+		//ifstream File("Id_index.bin", ios::in | ios::binary);
+		
+		cout << "미래가 안보여";
+		if (find(V_id.begin(), V_id.end(), id) == V_id.end()) {
+			//id가 없을때
+			overlap = 1;
+			send(client, "succeed", 8, 0);
+			//id가 없다는 메세지 send
+			//다음창
+
+			Id_Bytein = tool::Recv(client, id);
+			if (Id_Bytein <= 0) {
+				return -1;
+			}
+			Password_Bytein = tool::Recv(client, password);
+			if (Password_Bytein <= 0) {
+				return -1;
+			}
+			//vector에 정보 추가
+			V_id.push_back(id);
+			ofstream wFile("Id_index.bin",ios_base::binary);
+			unsigned V_idSize V_id.size();
+			wFile.write(reinterpret_cast<char *>(&V_idSize), sizeof(unsigned));
+			wFile.write(reinterpret_cast<char *>(&V_id[0]), V_id.size() * sizeof(string))
+			wFile.close();
+			//binary 저장
+		}
+		else {
+
+			send(client, "overlap", 8, 0);
+		}
+		
+			//id 중복 메세지 send
 	}while (overlap==0);
 
 
-
+	return 0;
 
 }
