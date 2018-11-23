2ec0c6a69040a0747a9013453a97db58758b8557
ysh4296 <dbtmdgns4296@naver.com>
2018-11-23 15:10:19 +0900

    Add files via upload

--- Project7/Signin.cpp
+++ Project7/Signin.cpp
@@ -19,59 +19,52 @@ int Signin::in(SOCKET client)
 	//id 중복검사
 	int overlap = 0;
 	vector<string> V_id;
-	set<pair<string, string>> user_Info;
+	string file_string;
+	map<string, string> user_Info;
 	do {
 		Id_Bytein = tool::Recv(client, id);
 		if (Id_Bytein <= 0) {
 			return -1;
 		}
-		if (V_id.size()) {
-			ifstream rFile("Id_index.bin", ios_base::binary);
-			unsigned vsize;
+		tool::TxtToVector("id_index.txt", V_id);
 
-			rFile.read(reinterpret_cast<char*>(&vsize), sizeof(unsigned));
-			vector<string> theId(vsize);
-			rFile.read(reinterpret_cast<char*>(&theId[0]), vsize * sizeof(string));
-			V_id = theId;
-			rFile.close();
-		}
-		//in_index 파일 불러오기(binary)
-		//ifstream File("Id_index.bin", ios::in | ios::binary);
+		//in_index 파일 불러오기(txt)
 
 		cout << "미래가 안보여";
 		if (find(V_id.begin(), V_id.end(), id) == V_id.end()) {
 			//id가 없을때
 			overlap = 1;
-			send(client, "succeed", 8, 0);
+			send(client, "succeed\nYou can use this id\nNow enter your password", 55, 0);
 			//id가 없다는 메세지 send
 			//다음창
 
-			Id_Bytein = tool::Recv(client, id);
+			/*Id_Bytein = tool::Recv(client, id);
 			if (Id_Bytein <= 0) {
-				return -1;
+			   return -1;
 			}
+			*/
+			//vector에 정보 추가
+			V_id.push_back(id);
 			Password_Bytein = tool::Recv(client, password);
 			if (Password_Bytein <= 0) {
 				return -1;
 			}
-			//vector에 정보 추가
-			V_id.push_back(id);
-			ofstream wFile("Id_index.bin", ios_base::binary);
-			unsigned V_idSize = V_id.size();
-			wFile.write(reinterpret_cast<char *>(&V_idSize), sizeof(unsigned));
-			wFile.write(reinterpret_cast<char *>(&V_id[0]), V_id.size() * sizeof(string));
-			wFile.close();
-			//binary 저장
+			tool::VectorToTxt("id_index.txt", V_id);
+
+			//txt파일 저장
 		}
 		else {
 
-			send(client, "overlap", 8, 0);
+			send(client, "overlap\nRe-enter your id\n", 27, 0);
 		}
 
 		//id 중복 메세지 send
+
 	} while (overlap == 0);
+	send(client, "Successfully finished signin", 29, 0);
+	user_Info.insert(pair<string, string>(id, password));
 
-
+	tool::MapToTxt("Id_Ps_map.txt", user_Info);
 	return 0;
 
 }
--- Project7/Tool.h
+++ Project7/Tool.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <WinSock2.h>
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <iterator>
 #define MAX_BUFFER_SIZE 4092
 using namespace std;
 class tool// recv와 send를 사용하기 쉽게 만들어줌
@@ -12,5 +20,8 @@ public:
 	~tool(); 
 	int Recv(SOCKET client, char buf[]);
 	void Send(SOCKET client, string msg);
-};
+	void VectorToTxt(const char* fileName, vector<string> &theId);
+	void TxtToVector(const char* fileName, vector<string> &theId);
+	void MapToTxt(const char* fileName, map<string, string> &user_Info);
 
+};
