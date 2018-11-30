3831d48c4883a8d613e6f79fbc86f0c99c9b796b
paxcho1 <paxcho1@naver.com>
2018-11-30 15:44:41 +0900

    Add files via upload

--- TimeTable.cpp
+++ TimeTable.cpp
@@ -48,19 +48,62 @@ public:
 					TimeLine[i][j] = 1;
 				}
 			}
+			Map_Schedule[Schedule] = Time;
+			cout << "added schedule";
 		}
-		Map_Schedule[Time] = Schedule;
+		
+		return 0;
 	}
-	int DeleteSchedule() {
+	int DeleteSchedule(string Schedule) {
+		int Shour, Sminute, Fhour, Fminute, Check;
+		vector<string>V_Time;
+		vector<string> V_Stime;
+		vector<string> V_Ftime;
+		vector<string>::iterator tIter = V_Time.begin();
+		vector<string>::iterator StIter = V_Stime.begin();
+		vector<string>::iterator FtIter = V_Ftime.begin();
+		
+		
+		
+		if (map<string, string>::iterator ScheduleIter = Map_Schedule.find(Schedule) == Map_Schedule.end()) {
+			cout << "there is no such Schedule"
+				return -1;
+		}
+		else {
+			
+			tool::splitString(V_time, ScheduleIter->second, '~');
+
+			tool::splitString(V_Stime, *(tIter), ':');
+			tool::splitString(V_Ftime, *(++tIter), ':');
+
+			Shour = stoi(*(StIter));
+			Sminute = stoi(*(++StIter));
+			Fhour = stoi(*(FtIter));
+			Fminute = stoi(*(++FtIter));
+			Map_Schedule.erase(Schedule);
+
+			for (int i = Shour; i < Fhour; i++) {
+				for (int j = Sminute; j < Fminute; j++) {
+					TimeLine[i][j] = 0;
+				}
+			}
+		}
+		return 0;
+	}
+	string FindSchedule() {
 
 	}
 	map<string,string> ReturnSchedule() {
-
+		return Map_Schedule;
+	}
+	int* ReturnTimeLine() {
+		return TimeLine;
 	}
 
 };
 int TimeTable::table(SOCKET client, string id) {
 	vector<DailySchedule> WeeklySchedule;
 	DailySchedule Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
-
+	
+	return 0;
 }
